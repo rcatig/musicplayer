4d6006e598b61d8b43fb7ef320cc449f7fc71597
Aira <rcatig@uci.edu>
2025-03-26 15:00:59 -0700

    Created get token function.

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-import requests
+import json
 import os
+import base64
 from dotenv import load_dotenv
+from requests import post
 
-def get_token():
+def get_token(client_id, client_secret):
+    auth_string = client_id  + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
 
 def run():
     load_dotenv()
     client_id = os.getenv("CLIENT_ID")
     client_secret = os.getenv("CLIENT_SECRET")
-    get_token()
+    print(get_token(client_id, client_secret))
 
-if __name__ == "main":
+if __name__ == '__main__':
     run()
