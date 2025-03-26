4e0268e88280adbc868fa9c7b958f2f77a684cf7
Aira <rcatig@uci.edu>
2025-03-26 14:42:31 -0700

    Created run function.

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+def get_token():
+
+def run():
+    load_dotenv()
+    client_id = os.getenv("CLIENT_ID")
+    client_secret = os.getenv("CLIENT_SECRET")
+    get_token()
+
+if __name__ == "main":
+    run()
