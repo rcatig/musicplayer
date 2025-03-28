0b52cc721e188cf2768380a9af9ddd3917fb4e40
Aira <rcatig@uci.edu>
2025-03-27 19:29:27 -0700

    Created get current track.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import json
 import os
 import base64
+import requests
 from dotenv import load_dotenv
 from requests import post
+from flask import Flask
+
+def create_flask(secret_key):
+    app = Flask(__name__)
+    app.secret_key = secret_key
 
 def get_token(client_id, client_secret):
     auth_string = client_id  + ":" + client_secret
@@ -22,11 +28,31 @@ def get_token(client_id, client_secret):
 
 def get_auth_header(token):
     return {"Authorization": "Bearer " + token}
+
+def get_current_track(token):
+    auth_header = get_auth_header(token)
+    url = "https://api.spotify.com/v1/me/player/currently-playing"
+    response = requests.get(
+        url,
+        headers=auth_header
+    )
+    resp_json = response.json()
+    print(resp_json)
+    track_type = resp_json["currently_playing_type"]
+    return track_type
+
 def run():
     load_dotenv()
+    secret_key = os.getenv("SECRET_KEY")
     client_id = os.getenv("CLIENT_ID")
     client_secret = os.getenv("CLIENT_SECRET")
-    print(get_token(client_id, client_secret))
+    redirect_uri = "https://localhost:5000/callback"
+    auth_url = "https://accounts.spotify.com/authorize"
+    token_url = "https://accounts.spotify.com/api/token"
+    api_base_url = "https://api.spotify.com/v1/"
+    token = get_token(client_id, client_secret)
+    print(token)
+    print(get_current_track(token))
 
 if __name__ == '__main__':
     run()
